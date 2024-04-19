d8864337cbae7bad29621121adc6016afe964dff
Alex Cristache <mc14468@my.bristo.ac.uk>
2024-04-19 23:27:38 +0300

    dynamic templates and custom render function

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 )
 
+func HomeHandler(c echo.Context) error {
+	c.Logger().Debug("got here")
+	return Render(c, http.StatusOK, Home())
+}
+
+func TimeHandler(ctx echo.Context) error {
+	//return TimeHandler{Now: now}
+	ctx.Logger().Debug("got here")
+	return Render(ctx, http.StatusOK, timeComponent(time.Now()))
+}
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	ctx.Response().Writer.WriteHeader(statusCode)
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+
+	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
+
 func main() {
 	app := echo.New()
-    logger := app.Logger
+	logger := app.Logger
 
-    logger.SetLevel(log.DEBUG)
+	logger.SetLevel(log.DEBUG)
 
-    app.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World")
-    })
-    app.Logger.Fatal(app.Start(":8080"))
+	app.GET("/", HomeHandler)
+	app.GET("/time", TimeHandler)
+	app.Logger.Fatal(app.Start(":8080"))
 }
